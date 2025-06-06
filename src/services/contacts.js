682f5c1c6b4053f198ef9bd6547f6fea57deb77e
amurovnaa <amurovna09@gmail.com>
2025-06-06 14:35:05 +0300

    all done

---
+++ src/services/contacts.js
@@ -0,0 +1,10 @@
+import { Contacts } from '../db/models/contacts.js';
+
+export const getContacts = async () => {
+  const contacts = await Contacts.find();
+  return contacts;
+};
+export const getContactById = async (contactId) => {
+  const contact = await Contacts.findById(contactId);
+  return contact;
+};
